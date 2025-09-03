b304e06674b9273584c8ab9e40327ff7968b0ff2
Brett <daybrett98@gmail.com>
2025-09-03 18:31:31 -0400

    coding again

---
+++ Module 02/madlib.py
@@ -0,0 +1,36 @@
+'''
+Mad Lib Program
+https://madlibs.com/wp-content/uploads/2024/06/VacationFun_ML_2009_pg15.pdf
+Author: Brett Day
+'''
+
+ADJECTIVE = "Enter an adjective: "
+NOUN = "Enter a Noun"
+PLURAL_NOUN = "Enter a Plugral Noun: "
+GAME = "Enter a Game: "
+ING_VERB = "Enter a verb ending in ING: "
+PLANT = "Enter a plant: "
+BODY_PART = "Enter a part of the body: "
+PLACE = "Enter a place: "
+NUMBER = "Enter a number: "
+
+adjective1 = input(ADJECTIVE)
+adjective2 = input(ADJECTIVE)
+noun1 = input(NOUN)
+noun2 = input(NOUN)
+plural_noun1 = input(PLURAL_NOUN)
+game = input(GAME)
+plural_noun2 = input(PLURAL_NOUN)
+verb1 = input(ING_VERB)
+verb2 = input(ING_VERB)
+plural_noun3 = input(PLURAL_NOUN)
+verb3 = input(ING_VERB)
+noun3 = input(NOUN)
+plant = input(PLANT)
+body_part = input(BODY_PART)
+place = input(PLACE)
+verb4 = input(ING_VERB)
+adjective3 = input(ADJECTIVE)
+number = input(NUMBER)
+plural_noun4 = input(PLURAL_NOUN)
+
