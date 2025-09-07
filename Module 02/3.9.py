cf7c04a989414e6de7465ad0aeb5b1ae04591fe4
Brett <daybrett98@gmail.com>
2025-09-07 17:06:27 -0400

    doing hw

---
+++ Module 02/3.9.py
@@ -0,0 +1,22 @@
+#  import math
+
+# x = math.fabs(3.1)
+# print(x)
+
+# import math
+
+# x = math.pow(3.0, 2.0)
+# print(x)
+
+# import math
+
+# x = math.fabs(-8.0 + 1.0)
+# print(x)
+
+# import math
+
+# x = math.pow(5.0, math.sqrt(4.0))
+# print(x)
+
+# print("Sara\\\nJoe\\nBeth\nAlex")	
+print(r"The escape sequence for single quote is \'")
